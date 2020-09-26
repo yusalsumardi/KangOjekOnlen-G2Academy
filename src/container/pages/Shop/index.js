700c77563086707792c648e6865353c6cb246036
nbl77 <amdnabil2001@gmail.com>
2020-09-26 21:55:26 +0700

    progress 50%

--- src/container/pages/Shop/index.js
+++ src/container/pages/Shop/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Shop from './ShopScreen';
 import DetailStore from './DetailStore';
+import Result from './Result';
 import Payment from './Payment';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -13,6 +14,7 @@ function App() {
         <Stack.Screen name="Shop" component={Shop} options={{headerShown:false}} />
         <Stack.Screen name="DetailStore" component={DetailStore} options={{headerShown:false}} />
         <Stack.Screen name="Payment" component={Payment} options={{headerShown:false}} />
+        <Stack.Screen name="Result" component={Result} options={{headerShown:false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
