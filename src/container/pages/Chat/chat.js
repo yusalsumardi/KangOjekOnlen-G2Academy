83c5593ddf8cec7f723b9acbe7b518a9cf76c211
rifqifakhirin <rifqifakhirin@gmail.com>
2020-09-26 21:33:10 +0700

    Chat bot dibuat

--- src/container/pages/Chat/chat.js
+++ src/container/pages/Chat/chat.js
@@ -5,6 +5,60 @@ import {
     ScrollView,
     Text
 } from "react-native"
+import ChatBot from 'react-native-chatbot'
+
+const steps = [
+    {
+        id: "1",
+        message: "Hai Kak, Terima kasih sudah memesan :)",
+        trigger: "2"
+    },
+    {
+        id: "2",
+        message: "Mohon ditunggu ya Kak (9^-^)9",
+        trigger: "3"
+    },
+    {
+        id: "3",
+        user: true,
+        trigger: "4"
+    },
+    {
+        id: "4",
+        message: "Siaaaap Meluncur",
+        trigger: "5"
+    },
+    {
+        id: "5",
+        user: true,
+        trigger: "6"
+    },
+    {
+        id: "6",
+        message: "Dekat Kak, maaf Kakak pakai baju apa ya?",
+        trigger: "7"
+    },
+    {
+        id: "7",
+        user: true,
+        trigger: "8"
+    },
+    {
+        id: "8",
+        message: "Oh iya sudah keliatan",
+        trigger: "9"
+    },
+    {
+        id: "9",
+        user: true,
+        trigger: "10"
+    },
+    {
+        id: "10",
+        message: "Siap2 berangkat Kak :)",
+        end: true
+    }
+]
 
 export default class Chat extends React.Component {
     constructor(props){
@@ -13,16 +67,17 @@ export default class Chat extends React.Component {
         }
     }
     render(){
-        return(
-            <ScrollView>
-                <View>
-                    <Text>Chat</Text>
-                </View>
-            </ScrollView>
+        return(            
+            <View style={styles.container}>
+                <ChatBot steps={steps} />
+            </View>
         )
     }
 }
 
 var styles = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        paddingTop: 20
+    }    
 })
