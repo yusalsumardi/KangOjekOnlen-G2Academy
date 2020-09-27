b50a2b105b08f94e050747a8e34bcd3947f3bfa0
rifqifakhirin <rifqifakhirin@gmail.com>
2020-09-27 17:35:52 +0700

    penambahan filedata.js

---
+++ src/container/pages/Shop/FileData.js
@@ -0,0 +1,106 @@
+const Data = [
+    {
+        toko: "Swager Shop",
+        keterangan: "Fashion & Accessories",
+        rating: "4.5",
+        alamat: "Kalibata Tengah",
+        imgtoko: "tokoA.jfif",
+        imgdetail: "detailToko.jpg",
+        produk: [
+            {
+                namprod: "Air Jordan",
+                ketprod: "Sneaker terbaik dan terlaris di kelasnya",
+                harprod: 200000,
+                imgprod: "A1.jfif",
+            },
+            {
+                namprod: "Navy Hat",
+                ketprod: "Navy Hat Blue terbaru dengan desain yang sejuk",
+                harprod: 50000,
+                imgprod: "A2.jfif",
+            },
+            {
+                namprod: "Long Sweater",
+                ketprod: "Sweater Hitam berbahan sejuk dan bahan kuat",
+                harprod: 100000,
+                imgprod: "A3.jfif",
+            },
+            {
+                namprod: "Long Pant",
+                ketprod: "Swag Long Pant berbahan kuat dan untuk semi tactical",
+                harprod: 150000,
+                imgprod: "A4.jfif",
+            }
+        ]
+    },
+    {
+        toko: "General Tech",
+        keterangan: "Electronics Stuff",
+        rating: "4.7",
+        alamat: "Pasar Minggu",
+        imgtoko: "tokoB.jfif",
+        imgdetail: "tokoBdetail.jfif",
+        produk: [
+            {
+                namprod: "Headset Gaming",
+                ketprod: "Headset dengan Sound Boosted sangat cocok untuk gaming",
+                harprod: 100000,
+                imgprod: "B1.jfif",
+            },
+            {
+                namprod: "Sound/Speaker",
+                ketprod: "Sound System dengan teknologi 4D Sound lebih terasa real",
+                harprod: 150000,
+                imgprod: "B2.jfif",
+            },
+            {
+                namprod: "Wifi Extender",
+                ketprod: "Extender yang dapat meningkatkan dan menguatkan jaringan Wifi",
+                harprod: 100000,
+                imgprod: "B3.jfif",
+            },
+            {
+                namprod: "Mouse Gaming",
+                ketprod: "Mouse yang didesain dengan respon yang sangat cepat",
+                harprod: 90000,
+                imgprod: "B4.jfif",
+            }
+        ]
+    },
+    {
+        toko: "Perfecto Cook",
+        keterangan: "Cooking Tools & Accessories",
+        rating: "4.2",
+        alamat: "Cililitan Besar",
+        imgtoko: "tokoC.jfif",
+        imgdetail: "tokoCdetail.jfif",
+        produk: [
+            {
+                namprod: "Utensil Set",
+                ketprod: "Alat masak yang kuat dan tahan panas",
+                harprod: 150000,
+                imgprod: "C1.jfif",
+            },
+            {
+                namprod: "Wajan Anti Gores",
+                ketprod: "Wajan licin, masakan tidak menempel, dan kuat",
+                harprod: 100000,
+                imgprod: "C2.jfif",
+            },
+            {
+                namprod: "Knife Set",
+                ketprod: "Pisau berbagai ukuran dengan tingkat ketajaman tinggi",
+                harprod: 110000,
+                imgprod: "C3.jfif",
+            },
+            {
+                namprod: "Panci Stainless",
+                ketprod: "Panci ukuran besar dengan daya tahan tinggi dengan penutup kaca",
+                harprod: 120000,
+                imgprod: "C4.jfif",
+            }
+        ]
+    }
+]
+
+export default Data;
